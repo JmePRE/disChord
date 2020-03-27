b66b00b3e74eb251e2c4eb864b95444bd293c12f
john chai <jc050402@gmail.com>
2020-03-27 13:11:03 +0800

    Add files via upload
    train

--- stuff/train_script.py
+++ stuff/train_script.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from dsp_preprocess import label_process
 from keras.optimizers import Adam
 import os
+import pickle
 '''
 import argparse
 
@@ -28,6 +29,8 @@ model = Sequential()
 model.add(Dense(units=12, activation='sigmoid', input_shape=(12,)))
 model.add(Dense(48, activation='relu'))
 model.add(Dropout(0.1))
+model.add(Dense(48, activation='relu'))
+model.add(Dropout(0.1))
 model.add(Dense(15, activation='sigmoid'))
 
 # try using different optimizers and different optimizer configs
@@ -35,19 +38,33 @@ model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-xt, yt = np.zeros(12), np.zeros(15)
+if not (os.path.exists('xt.npy')):
+    xt, yt = np.zeros(12), np.zeros(15)
+    donefiles = []
+    print('die')
+else:
+    donefiles = pickle.load(open('donefiles.pkl', 'rb'))
+    xt, yt = np.load('xt.npy'), np.load('yt.npy')
+    print(xt.shape)
+    print(yt.shape)
+
 for(root, dir, file) in os.walk('data/labels/'):
     for f in file:
-        x0, y0 = label_process(("data/labels/"+f), ("data/soundfiles/"+f[:-4]+".wav"))
+        if (f not in donefiles):
+            x0, y0 = label_process(("data/labels/"+f), ("data/soundfiles/"+f[:-4]+".wav"))
+            donefiles.append(f)
+            print(xt.shape)
+            print(yt.shape)
+            print(x0.shape)
+            print(y0.shape)
+            xt = np.vstack([x0, xt])
+            yt = np.vstack([y0, yt])
 
-        print(xt.shape)
-        print(yt.shape)
-        print(x0.shape)
-        print(y0.shape)
-        xt = np.vstack([x0, xt])
-        yt = np.vstack([y0, yt])
 print(xt.shape)
 print(yt.shape)
+pickle.dump(donefiles, open('donefiles.pkl', 'wb'))
+np.save('xt.npy', xt)
+np.save('yt.npy', yt)
 # train the network
 print("[INFO] training network...")
 H = model.fit(
